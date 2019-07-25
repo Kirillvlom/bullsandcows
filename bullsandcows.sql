9f198fece642aecfc8a3c72960f7f20496ce8467
Kirillvlom <kirill.tolelev@mail.ru>
2019-07-25 13:59:44 +0300

    Изображения для инструкции

--- bullsandcows.sql
+++ bullsandcows.sql
@@ -11,7 +11,7 @@
  Target Server Version : 50719
  File Encoding         : 65001
 
- Date: 24/07/2019 16:06:40
+ Date: 25/07/2019 13:58:53
 */
 
 SET NAMES utf8mb4;
@@ -28,19 +28,20 @@ CREATE TABLE `resultTable`  (
   `time` text CHARACTER SET utf8 COLLATE utf8_general_ci NULL,
   `typeGame` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 23 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 24 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of resultTable
 -- ----------------------------
 INSERT INTO `resultTable` VALUES (14, 'Pechenka.io', '4', '17', '1');
-INSERT INTO `resultTable` VALUES (15, 'Pechenka.io', '1', '4', '3');
-INSERT INTO `resultTable` VALUES (16, 'Pechenka.io', '4', '15', '2');
-INSERT INTO `resultTable` VALUES (17, 'Артемка', '2', '65', '1');
-INSERT INTO `resultTable` VALUES (18, 'Pechenka.io', '10', '175', '3');
-INSERT INTO `resultTable` VALUES (19, 'Pechenka.io', '1', '17', '1');
-INSERT INTO `resultTable` VALUES (20, 'Pechenka.io', '1', '1256', '1');
-INSERT INTO `resultTable` VALUES (21, 'Pechenka.io', '2', '8', '1');
+INSERT INTO `resultTable` VALUES (15, 'Valeria', '1', '4', '3');
+INSERT INTO `resultTable` VALUES (16, 'User', '4', '15', '2');
+INSERT INTO `resultTable` VALUES (17, 'Test', '2', '65', '1');
+INSERT INTO `resultTable` VALUES (18, 'Olga', '10', '175', '3');
+INSERT INTO `resultTable` VALUES (19, 'Максим', '1', '17', '1');
+INSERT INTO `resultTable` VALUES (20, 'oduvanchic.io', '1', '1256', '1');
+INSERT INTO `resultTable` VALUES (21, 'zevs.io', '2', '8', '1');
 INSERT INTO `resultTable` VALUES (22, 'Тестовый игрок', '4', '11', '2');
+INSERT INTO `resultTable` VALUES (23, ' Pechenka 2', '9', '70', '2');
 
 SET FOREIGN_KEY_CHECKS = 1;
